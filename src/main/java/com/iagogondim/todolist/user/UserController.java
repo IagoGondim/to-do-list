83a9efad2f2977022b30f09debf5b12d561212e0
IagoGondim <iago-gondim@gotmail.com>
2023-10-10 16:18:14 -0300

    add IUserRepository

--- src/main/java/com/iagogondim/todolist/user/UserController.java
+++ src/main/java/com/iagogondim/todolist/user/UserController.java
@@ -1,6 +1,7 @@
 package com.iagogondim.todolist.user;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+  @Autowired
+  private IUserRepository userRepository;
+
   @PostMapping("/")
-  public void create(@RequestBody UserModel userModel) {
-    System.out.println(userModel.getUsername());
-    System.out.println(userModel.getName());
-    System.out.println(userModel.getPassword());
-    System.out.println("---------------------------");
+  public UserModel create(@RequestBody UserModel userModel) {
+    var userCreated = this.userRepository.save(userModel);
+    return userCreated;
 
   }
 
