804ece8a28ba5f5331e82b161ac685f8974b6983
IagoGondim <iago-gondim@gotmail.com>
2023-10-10 09:39:33 -0300

    project created

---
+++ src/test/java/com/iagogondim/todolist/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iagogondim.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
