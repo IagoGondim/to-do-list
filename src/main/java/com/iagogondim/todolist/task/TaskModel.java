ee494f6914b4289f00e3b99e1fbe417d3adcdabc
IagoGondim <iago-gondim@gotmail.com>
2023-10-11 10:07:06 -0300

    add tb_tasks

--- src/main/java/com/iagogondim/todolist/task/TaskModel.java
+++ src/main/java/com/iagogondim/todolist/task/TaskModel.java
@@ -1,4 +1,4 @@
-package com.iagogondim.todolist.user;
+package com.iagogondim.todolist.task;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -11,18 +11,19 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
-@Entity(name = "tb_users")
-public class UserModel {
+@Entity(name = "tb_tasks")
+public class TaskModel {
 
   @Id
   @GeneratedValue(generator = "UUID")
   private UUID id;
-
-  @Column(unique = true)
-  private String username;
-  private String name;
-  private String password;
+  private String description;
+  @Column(length = 50)
+  private String title;
+  private LocalDateTime startAt;
+  private LocalDateTime endAt;
+  private String priority;
   @CreationTimestamp
   private LocalDateTime createdAt;
-
+  private UUID idUser;
 }
